56e8286f052ce48b803020468aa0f8ed212038b3
jl2569 <jl2569@nau.edu>
2016-04-10 15:30:58 -0700

    New Schedule
    Used jquery to make new schedule but it still has no functionality

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -1,15 +1,26 @@
+var Days = ["","Mon","Tue","Wed","Thur","Fri","Sat","Sun"];
+var Time =["1:00 A.M.","1:30 A.M.","2:00 A.M.","2:30 A.M.","3:00 A.M.","3:30 A.M.","4:00 A.M.","4:30 A.M.","5:00 A.M.","5:30 A.M.","6:00 A.M.","6:30 A.M.","7:00 A.M.","7:30 A.M.","8:00 A.M.","8:30 A.M.","9:00 A.M.","9:30 A.M.","10:00 A.M.","10:30 A.M.","11:00 A.M.","11:30 A.M.","12:00 P.M.","12:30 P.M.","1:00 P.M.","1:30 P.M.","2:00 P.M.","2:30 P.M.","3:00 P.M.","3:30 P.M.","4:00 P.M.","4:30 P.M.","5:00 P.M.","5:30 P.M.","6:00 P.M.","6:30 P.M.","7:00 P.M.","7:30 P.M.","8:00 P.M.","8:30 P.M.","9:00 P.M.","9:30 P.M.","10:00 P.M.","10:30 P.M.","11:00 P.M.","11:30 P.M.","12:00 A.M.","12:30 A.M."];
 
-if (Meteor.isClient){
 
+// creates the table
+$('#dynamictable').append('<tr></tr>');
+var table = $('#dynamictable').children();
 
-Template.fullcalendar.rendered = function() {
-    var div = this.$(this.firstNode);
-    if(this.data != null) {
-        //jquery takes care of undefined values, no need to check here
-        div.attr('id', this.data.id);
-        div.addClass(this.data.class);
+for(i=0;i<Time.length+1;i++){
+    for(j=0;j<Days.length;j++){
+        if(i==0 )
+            table.append('<td>'+Days[j]+'</td>');
+        else{
+            if(j!=0)
+                table.append('<td><button></button></td>');
+            else
+                table.append('<td>'+Time[i-1]+'</td>');
+        }
     }
-    div.fullCalendar(this.data);
-};
+    table.append('<tr></tr>');
+}
 
-};
+
+
+
+
